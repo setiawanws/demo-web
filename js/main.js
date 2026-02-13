12a6e0533bba7bc5cbef4664a1236d5451b9ecf5
setiawanws <setiawanws.me@gmail.com>
2026-02-13 16:05:41 +0700

    x

--- js/main.js
+++ js/main.js
@@ -16,26 +16,26 @@
   $(document).ready(function(){  
 
     //active menu
-    // $(document).on("scroll", onScroll);
+    $(document).on("scroll", onScroll);
  
-    // $('a[href^="#"]').on('click', function (e) {
-    //   e.preventDefault();
-    //   $(document).off("scroll");
+    $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
  
-    //   $('a').each(function () {
-    //     $(this).removeClass('active');
-    //   })
-    //   $(this).addClass('active');
+      $('a').each(function () {
+        $(this).removeClass('active');
+      })
+      $(this).addClass('active');
  
-    //   var target = this.hash;
-    //   $target = $(target);
-    //   $('html, body').stop().animate({
-    //     'scrollTop': $target.offset().top+2
-    //   }, 500, 'swing', function () {
-    //     window.location.hash = target;
-    //     $(document).on("scroll", onScroll);
-    //   });
-    // });
+      var target = this.hash;
+      $target = $(target);
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top+2
+      }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+      });
+    });
 
     
     //scroll js
@@ -50,85 +50,84 @@
     });
 
     //menu
-    // var bodyEl = document.body,
-    // content = document.querySelector( '.content-wrap' )
-    // content = document.querySelector( '.content-wrap' ),
-    // openbtn = document.getElementById( 'open-button' ),
-    // closebtn = document.getElementById( 'close-button' ),
-    // isOpen = false;
+    var bodyEl = document.body,
+    content = document.querySelector( '.content-wrap' ),
+    openbtn = document.getElementById( 'open-button' ),
+    closebtn = document.getElementById( 'close-button' ),
+    isOpen = false;
 
-    // function inits() {
-    //   initEvents();
-    // }
+    function inits() {
+      initEvents();
+    }
 
-    // function initEvents() {
-    //   // openbtn.addEventListener( 'click', toggleMenu );
-    //   if( closebtn ) {
-    //     closebtn.addEventListener( 'click', toggleMenu );
-    //   }
+    function initEvents() {
+      openbtn.addEventListener( 'click', toggleMenu );
+      if( closebtn ) {
+        closebtn.addEventListener( 'click', toggleMenu );
+      }
 
-    //   // close the menu element if the target it´s not the menu element or one of its descendants..
-    //   content.addEventListener( 'click', function(ev) {
-    //     var target = ev.target;
-    //     if( isOpen && target !== openbtn ) {
-    //       toggleMenu();
-    //     }
-    //   } );
-    // }
+      // close the menu element if the target it´s not the menu element or one of its descendants..
+      content.addEventListener( 'click', function(ev) {
+        var target = ev.target;
+        if( isOpen && target !== openbtn ) {
+          toggleMenu();
+        }
+      } );
+    }
 
-    // function toggleMenu() {
-    //   if( isOpen ) {
-    //     classie.remove( bodyEl, 'show-menu' );
-    //   }
-    //   else {
-    //     classie.add( bodyEl, 'show-menu' );
-    //   }
-    //   isOpen = !isOpen;
-    // }
+    function toggleMenu() {
+      if( isOpen ) {
+        classie.remove( bodyEl, 'show-menu' );
+      }
+      else {
+        classie.add( bodyEl, 'show-menu' );
+      }
+      isOpen = !isOpen;
+    }
 
-    // inits();
+    inits();
 
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is Setiawan", "I'm a Web Programmer", "Love Simplicity"],
+        strings: ["I'm MR X ", "I'm a Web Developer", "Love Simplicity"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
         loop: true
     });
 
-    // owl carousel
-    // $('.owl-carousel').owlCarousel({
-    //   autoPlay: 3000, //Set AutoPlay to 3 seconds
+    //owl carousel
+    $('.owl-carousel').owlCarousel({
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
  
-    //   items : 1,
-    //   itemsDesktop : [1199,1],
-    //   itemsDesktopSmall : [979,1],
-    //   itemsTablet : [768,1],
-    //   itemsMobile : [479,1],
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1],
+      itemsTablet : [768,1],
+      itemsMobile : [479,1],
 
-    //   // CSS Styles
-    //   baseClass : "owl-carousel",
-    //   theme : "owl-theme"
-    // });
+      // CSS Styles
+      baseClass : "owl-carousel",
+      theme : "owl-theme"
+    });
 
-    // $('.owl-carousel2').owlCarousel({
-    //   autoPlay: 3000, //Set AutoPlay to 3 seconds
+    $('.owl-carousel2').owlCarousel({
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
  
-    //   items : 1,
-    //   itemsDesktop : [1199,1],
-    //   itemsDesktopSmall : [979,1],
-    //   itemsTablet : [768,1],
-    //   itemsMobile : [479,1],
-    //   autoPlay : false,
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1],
+      itemsTablet : [768,1],
+      itemsMobile : [479,1],
+      autoPlay : false,
 
-    //   // CSS Styles
-    //   baseClass : "owl-carousel",
-    //   theme : "owl-theme"
-    // });
+      // CSS Styles
+      baseClass : "owl-carousel",
+      theme : "owl-theme"
+    });
 
-    contact
+    //contact
     $('input').blur(function() {
 
       // check if the input has any value (if we've typed into it)
@@ -178,22 +177,22 @@
       });
     }
 
-  // window.onload = inits();
+  window.onload = inits();
 
   //nav-active
-  // function onScroll(event){
-  //   var scrollPosition = $(document).scrollTop();
-  //   $('.menu-list a').each(function () {
-  //     var currentLink = $(this);
-  //     var refElement = $(currentLink.attr("href"));
-  //     if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-  //       $('.menu-list a').removeClass("active");
-  //       currentLink.addClass("active");
-  //     }
-  //     else{
-  //       currentLink.removeClass("active");
-  //     }
-  //   });
-  // }
+  function onScroll(event){
+    var scrollPosition = $(document).scrollTop();
+    $('.menu-list a').each(function () {
+      var currentLink = $(this);
+      var refElement = $(currentLink.attr("href"));
+      if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+        $('.menu-list a').removeClass("active");
+        currentLink.addClass("active");
+      }
+      else{
+        currentLink.removeClass("active");
+      }
+    });
+  }
 
 })(jQuery);
